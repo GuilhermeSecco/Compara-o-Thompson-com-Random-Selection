a10010eb32aa15e03a36ebee9b8a3b1d40994790
GuilhermeSecco <155193683+GuilhermeSecco@users.noreply.github.com>
2025-05-15 06:55:22 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,73 +2,66 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-# Parâmetros
-rates = [0.05, 0.13, 0.09, 0.16, 0.11, 0.04, 0.20, 0.08, 0.01]
+rates = [0.05,0.13,0.09,0.16,0.11,0.04,0.20,0.08,0.01]
 N = 10000
 d = len(rates)
 
-
-def simulate_environment(rates, N):
+def ambiente(rates, N):
     d = len(rates)
     X = np.zeros((N, d))
     for i in range(N):
         for j in range(d):
             if np.random.rand() < rates[j]:
-                X[i][j] = 1
+                X[i][j] += 1
     return X
 
-
-def random_selection(X):
+def randomSelection(X):
     N, d = X.shape
-    strategies_selected = []
-    total_reward = 0
-    for n in range(N):
+    strategiesSelected = []
+    totalReward = 0
+    for i in range(N):
         strategy = random.randrange(d)
-        reward = X[n, strategy]
-        total_reward += reward
-        strategies_selected.append(strategy)
-    return total_reward, strategies_selected
+        reward = X[i, strategy]
+        totalReward += reward
+        strategiesSelected.append(strategy)
+    return totalReward, strategiesSelected
 
-
-def thompson_sampling(X):
+def thompsonSampling(X):
     N, d = X.shape
-    strategies_selected = []
-    total_reward = 0
-    numbers_of_rewards_1 = [0] * d
-    numbers_of_rewards_0 = [0] * d
+    strategiesSelected = []
+    totalReward = 0
+    reward1 = [0]*d
+    reward0 = [0]*d
 
-    for n in range(N):
-        max_random = 0
-        strategy = 0
-        for i in range(d):
-            random_beta = random.betavariate(numbers_of_rewards_1[i] + 1, numbers_of_rewards_0[i] + 1)
-            if random_beta > max_random:
-                max_random = random_beta
-                strategy = i
-        reward = X[n, strategy]
+    for i in range(N):
+        select = 0
+        maxRandom = 0
+        for j in range(d):
+            maxBeta = np.random.beta(reward1[j]+1,reward0[j]+1)
+            if maxBeta > maxRandom:
+                maxRandom = maxBeta
+                select = j
+        reward = X[i][select]
 
         if reward == 1:
-            numbers_of_rewards_1[strategy] += 1
+            reward1[select] += 1
         else:
-            numbers_of_rewards_0[strategy] += 1
+            reward0[select] += 1
 
-        total_reward += reward
-        strategies_selected.append(strategy)
+        totalReward += reward
+        strategiesSelected.append(select)
 
-    return total_reward, strategies_selected
+    return totalReward, strategiesSelected
 
+X = ambiente(rates, N)
+totalRewardRS, strategiesSelectedRS = randomSelection(X)
+totalRewardTS, strategiesSelectedTS = thompsonSampling(X)
 
-X = simulate_environment(rates, N)
-total_reward_rs, strategies_selected_rs = random_selection(X)
-total_reward_ts, strategies_selected_ts = thompson_sampling(X)
+relativeReturn = (totalRewardTS - totalRewardRS) / totalRewardRS * 100
+print("Thompson se saiu {:.0f}%".format(relativeReturn), 'melhor que o Random Selection')
 
-
-relative_return = (total_reward_ts - total_reward_rs) / total_reward_rs * 100
-print("Relative Return: {:.0f} %".format(relative_return))
-
-
-plt.hist(strategies_selected_ts)
-plt.title('Histogram of selections of Thompson Sampling')
-plt.xlabel('Strategy')
-plt.ylabel('Number of times the strategy was selected')
+plt.hist(strategiesSelectedTS)
+plt.title('Histograma das seleções do Thompson Sampling')
+plt.xlabel('Estratégias')
+plt.ylabel('Número de vezes que a estratégia foi selecionada')
 plt.show()
